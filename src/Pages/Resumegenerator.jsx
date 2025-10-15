298bc207367b7331f673cefbac3f260f4cef3f54
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-15 17:27:31 +0530

    pages and components added with files

---
+++ src/Pages/Resumegenerator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Resumegenerator() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Resumegenerator
---
+++ src/components/Privew.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Privew() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Privew
