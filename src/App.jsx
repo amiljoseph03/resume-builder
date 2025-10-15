e2c892a0dc929874b4f782665da243306cc83ec3
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-15 17:00:51 +0530

    material ui installed and button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+
+import Button from '@mui/material/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1>Resume Builder</h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
-  )
+  );
 }
 
 export default App
