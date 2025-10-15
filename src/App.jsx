86816c991b312ca2a3a12be2e58a466685eaadf8
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-15 17:14:23 +0530

    react icon added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 import Button from '@mui/material/Button';
+import { FaHome } from 'react-icons/fa';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Resume Builder</h1>
+      <h1 className='text-primary' > <FaHome />Resume Builder</h1>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
