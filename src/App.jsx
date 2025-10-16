1da0fb2eb87cad3f4bea9f7aa9057eb2d0960c5a
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-16 16:06:07 +0530

    react router dom installed

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,26 @@ import './App.css'
 import Button from '@mui/material/Button';
 import { FaHome } from 'react-icons/fa';
 
+import Header from `components./Header`
+import Footer from `components./Footer`
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1 className='text-primary' > <FaHome />Resume Builder</h1>
+      {/* <h1 className='text-primary' > <FaHome />Resume Builder</h1>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="outlined">Outlined</Button> */}
+
+
+
+
+      <Header/>
+      <Footer/>
     </>
   );
 }
