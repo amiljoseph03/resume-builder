d09256fa052482de5b232ea7c89d6f89fc793bf8
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-22 16:01:44 +0530

    history created

--- src/Pages/History.jsx
+++ src/Pages/History.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
+import { MdDelete } from "react-icons/md"
+import { Link } from 'react-router-dom';
+import { Paper, Button, Box } from '@mui/material';
+
+
+
 function History() {
   return (
     <div>
-      <h1>history</h1>
+      <h1>Downloaded resume history</h1>
+      <Link to={'/'} style={{ marginTop: '-50px' }} className="float-end me-5">
+        BACK
+      </Link>
+      <Box component="section" className="container-fluid">
+        <div className="row">
+          <div className="col-md-4">
+            <Paper elevation={3} sx={{ my: 5, p: 5, textAlign: 'center' }}>
+              <div className="d-flex align-items-center justify-content-evenly ">
+                <h6>
+                  Review At : <br />
+                  Item
+                </h6>
+                <button className='btn text-danger fs-4 ms-5'>
+                  <MdDelete />
+                </button>
+              </div>
+
+              <div className="border rounded p-3">
+                <img className='img-fluid' src="" alt="resume" />
+              </div>
+            </Paper>
+          </div>
+        </div>
+      </Box>
     </div>
-  )
+  );
 }
 
 export default History
