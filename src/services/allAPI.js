577ecec3c0409fdfbec82c67a7da8af284fdf363
amiljoseph03 <amiljoseph03@gmail.com>
2025-10-30 17:11:51 +0530

    api's added

--- src/services/allAPI.js
+++ src/services/allAPI.js
@@ -5,3 +5,24 @@ import commonAPI from "./commonAPI"
  export const addResumeAPI = async (resume)=>{
     return await commonAPI('POST',`${baseUrl}/resumes`,resume)
 }
+
+//editttttttttt resume api - PUT
+export const editResumeAPI=async(id,resume)=>{
+    return await commonAPI('PUT', `${baseUrl}/resumes/${id}`,resume)
+}
+
+//add download history - POST
+export const addDownloadHistoryAPI=async(resume)=>{
+    return await commonAPI('POST',`${baseUrl}/history`,resume)
+}
+
+
+//get history - POST
+export const getHistoryAPI=async()=>{
+    return await commonAPI('GET',`${baseUrl}/history`,{})
+}
+
+//delete history - POST
+export const deleteHistoryAPI=async(id)=>{
+    return await commonAPI('DELETE',`${baseUrl}/history`,{})
+}
